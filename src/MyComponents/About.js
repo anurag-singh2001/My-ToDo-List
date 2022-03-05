641b73ed0d17a47c2a55f9dcd6b8b4290215d0f9
anurag-singh2001 <anuragrazarwal@gmail.com>
2022-03-06 02:03:41 +0530

    first commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum, illo at itaque, similique labore voluptatem perspiciatis nam sunt sit cum dignissimos qui iusto quo ut? Odio facilis nam atque at!</p>
+        </div>
+    )
+}
